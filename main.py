30bbf697004fdbdf3b0890e4db77bdc6e04ea066
Applify Lab inc <tosyno826@gmail.com>
2025-03-28 16:30:57 -0700

    New Commit

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from sqladmin import Admin, ModelView
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
+engine = create_engine(
+    "sqlite:///example.db",
+    connect_args={"check_same_thread": False},
+)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+Base.metadata.create_all(engine)  # Create tables
 app = FastAPI()
+admin = Admin(app, engine)
 
+
+class UserAdmin(ModelView, model=User):
+    column_list = [User.id, User.name]
+
+
+admin.add_view(UserAdmin)
 
 @app.get("/")
 async def root():
